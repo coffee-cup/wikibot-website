c12ac8f846884485caa256ccca0ae83b500a56f7
Jake Runzer <jakerunzer@gmail.com>
2017-11-07 15:13:15 -0800

    server build dir with node

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var serveStatic = require('serve-static');
 var WikiFakt = require('wikifakt');
 
 var PORT = process.env.PORT || 9090;
@@ -7,6 +8,14 @@ var app = express();
 var corsOption = {
   origin: 'http://localhost:8080'
 };
+
+app.use(serveStatic(__dirname + '/build'));
+
+app.get('/', cors(corsOption), function(req, res) {
+  res.sendFile('build/index.html', {
+    root: __dirname
+  });
+});
 
 app.get('/fakt', cors(corsOption), function(req, res) {
   WikiFakt.getRandomFact()
